a29974d12140b4f9b52d112eb714b56c7149aebb
Aryan Sharma <sharmaisgreat46@gmail.com>
2022-12-14 03:51:16 +0530

    Done many changes

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -20,17 +20,17 @@ export const navLinks = [
 export const socialLinks = [
   {
     label: 'Twitter',
-    url: 'https://twitter.com/hamishMW',
+    url: 'https://twitter.com/aryansharma9917',
     icon: 'twitter',
   },
-  {
-    label: 'Figma',
-    url: 'https://www.figma.com/@Hamish',
-    icon: 'figma',
-  },
+  // {
+  //   label: 'Figma',
+  //   url: 'https://www.figma.com/@Hamish',
+  //   icon: 'figma',
+  // },
   {
     label: 'Github',
-    url: 'https://github.com/HamishMW',
+    url: 'https://github.com/aryansharma9917',
     icon: 'github',
   },
 ];
--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -18,7 +18,7 @@ import { ProjectSummary } from 'layouts/Home/ProjectSummary';
 import { useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 
-const disciplines = ['Developer', 'Prototyper', 'Animator', 'Illustrator', 'Modder'];
+const disciplines = ['Developer', 'OpenSource Contributor', 'DevOps'];
 
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
@@ -68,8 +68,8 @@ export const Home = () => {
   return (
     <div className={styles.home}>
       <Meta
-        title="Designer + Developer"
-        description="Design portfolio of Hamish Williams — a product designer working on web & mobile
+        title="Developer + OpenSource Contributor "
+        description="Design portfolio of Aryan Sharma — a product designer working on web & mobile
           apps with a focus on motion, experience design, and accessibility."
       />
       <Intro
