a7ed058db75a74e21439510188038dd05a311aed
Aryan Sharma <sharmaisgreat46@gmail.com>
2023-01-09 01:34:42 +0530

    Modification

--- src/pages/uses/Uses.js
+++ src/pages/uses/Uses.js
@@ -45,7 +45,7 @@ export const Uses = () => {
                     <Link href="https://www.figma.com">Figma</Link> is my primary tool for
                     UI design these days. Made the switch from Sketch in 2020 and haven’t
                     looked back. I’ve also created{' '}
-                    <Link href="https://www.figma.com/@hamish">a few plugins</Link> that
+                    <Link href="https://www.figma.com/@aryansharma">a few plugins</Link> that
                     you can install.
                   </ListItem>
                   <ListItem>
